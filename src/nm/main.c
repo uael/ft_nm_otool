d5c53ad958930babc2b4da0fa6caa0e50b5a5b93
uael <lucas.abel@yandex.com>
2018-11-15 12:00:19 +0100

    make

--- src/nm/main.c
+++ src/nm/main.c
@@ -1,5 +1,10 @@
 #include <libft.h>
 
+#include <mach-o/loader.h>
+#include <mach-o/nlist.h>
+#include <mach-o/stab.h>
+#include <mach-o/reloc.h>
+
 int main(void)
 {
 	ft_printf("Hello from nm !\n");
