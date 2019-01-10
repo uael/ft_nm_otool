a3418aae50b068782a0ef6a4eea5de1bf6153028
uael <lucas.abel@yandex.com>
2019-01-10 16:42:57 +0100

    hell tests

--- test/test.fish
+++ test/test.fish
@@ -1,14 +1,25 @@
 # Go to project root dir
 cd dirname (status -f)/..;
 
+make SAN=1 DEBUG=1
 mkdir -p out
 
 for i in (cat test/*.txt)
     echo $i
-    ./build/bin/ft_nm $i > out/a
+    
+    ./build/bin/ft_nm-san  $i > out/a
+    set st1 $status;
     nm $i > out/b
-    diff -B out/a out/b
-    if test "$status" != 0
+    set st2 $status;
+
+    if test $st1 != $st2
         break
+    end
+
+    if test $st1 -eq 0
+        diff -B out/a out/b
+        if test "$status" != 0
+            break
+        end
     end
 end
